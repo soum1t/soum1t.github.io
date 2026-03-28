6d269622b27ee1c0617166750254760e9cce1e11
Soumit Satiar <soumitsatiar@gmail.com>
2026-03-28 21:48:05 +0530

    added tailwind and a font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, fontProviders } from 'astro/config'
 
 export default defineConfig({
   site: 'https://soum1t.github.io',
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  fonts: [{
+    provider: fontProviders.fontsource(),
+    name: "JetBrains Mono",
+    cssVariable: "--font-jetbrains-mono",
+  }]
 })
